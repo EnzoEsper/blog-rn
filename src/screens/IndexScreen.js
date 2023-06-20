0a821693ff4d8e4fc6181a716d6d4a292de499b6
Firstname Lastname <enzoesper7@gmail.com>
2023-06-20 04:59:58 -0300

    initial setup and adding navigation

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function IndexScreen() {
+  return <View>
+    <Text>Index Screen</Text>
+  </View>
+}
+
+styles = StyleSheet.create({})
+
+export default IndexScreen;
