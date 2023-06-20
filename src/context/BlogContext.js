092d9518053820fa1ae88de2d3d430555c295209
Firstname Lastname <enzoesper7@gmail.com>
2023-06-20 05:32:01 -0300

    modifying the static state and adding useState with a helper function to add a new post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,9 +1,14 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const BlogContext = createContext();
 
 export const BlogProvider = ({ children }) => {
-  const blogPosts = [{ title: 'Blog Post #1' }, { title: 'Blog Post #2' }];
+  // const blogPosts = [{ title: 'Blog Post #1' }, { title: 'Blog Post #2' }];
+  const [blogPosts, setBlogPosts] = useState([]);
 
-  return <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>;
+  const addBlogPost = () => {
+    setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }]);
+  };
+
+  return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>{children}</BlogContext.Provider>;
 };
