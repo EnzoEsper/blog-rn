6fdd1d93bdee7b995a8392f0399c367443b6f73d
Firstname Lastname <enzoesper7@gmail.com>
2023-07-09 16:39:51 -0300

    deleting posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -5,11 +5,11 @@ const blogReducer = (state, action) => {
   switch (action.type) {
     case 'get_blogposts':
       return action.payload;
-    case 'add_blogpost':
-      return [
-        ...state,
-        { id: Math.floor(Math.random() * 99999), title: action.payload.title, content: action.payload.content },
-      ];
+    // case 'add_blogpost':
+    //   return [
+    //     ...state,
+    //     { id: Math.floor(Math.random() * 99999), title: action.payload.title, content: action.payload.content },
+    //   ];
     case 'delete_blogpost':
       return state.filter((blogPost) => blogPost.id !== action.payload);
     case 'edit_blogpost':
@@ -37,7 +37,8 @@ const addBlogPost = (dispatch) => {
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({ type: 'delete_blogpost', payload: id });
   };
 };
